6578dfc7bf09254e5af94bf4c2f545ef2957f1c8
epgtechteam <epg.techteam@gmail.com>
2025-02-07 11:57:17 -0800

    feat: iframe integration good to go (#7)
    * feat: iframe integration good to go
    
    * feat: fix build
    
    * fix: ignore no-sync-scripts performance linting
    
    * fix: update widget container to jsx
    
    ---------
    
    Co-authored-by: George Mbugua <george_mbugua@intuit.com>

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -4,55 +4,55 @@
 import OfferPageWrapper from "@/components/OfferPageWrapper";
 
 async function getBearerToken() {
-  try {
-    const clientID = process.env.CLIENT_ID;
-    const clientSecret = process.env.CLIENT_SECRET;
+    try {
+        const clientID = process.env.CLIENT_ID;
+        const clientSecret = process.env.CLIENT_SECRET;
 
-    if (!clientID || !clientSecret) {
-      return { error: "Missing credentials", status: 500 };
+        if (!clientID || !clientSecret) {
+            return { error: "Missing credentials", status: 500 };
+        }
+
+        const credentials = `${clientID}:${clientSecret}`;
+        const body = new URLSearchParams();
+        body.append("grant_type", "client_credentials");
+        body.append("scope", "com.intuit.quickbooks.accounting");
+
+        const options = {
+            headers: {
+                Authorization:
+                    "Basic " + Buffer.from(credentials).toString("base64"),
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body,
+            method: "POST",
+        };
+
+        const response = await fetch(
+            `https://oauth-sandbox.platform.intuit.com/oauth2/v1/tokens/bearer`,
+            options
+        );
+
+        if (!response.ok) {
+            return { error: "Failed to fetch token", status: 500 };
+        }
+
+        const data = await response.json();
+        await new Promise((resolve) => {
+            // Added 3 seconds to test the server side rendering and display of loader
+            setTimeout(() => {
+                resolve("");
+            }, 5000);
+        });
+        return { status: 200, token: data.access_token };
+    } catch (error) {
+        console.log(JSON.stringify(error));
+        return { error: "Failed to fetch token", status: 500 };
     }
-
-    // const credentials = `${clientID}:${clientSecret}`;
-    const body = new URLSearchParams();
-    body.append("grant_type", "client_credentials");
-    body.append("scope", "lending.offers.test");
-    body.append("client_id", clientID);
-    body.append("client_secret", clientSecret);
-
-    const options = {
-      headers: {
-        // Authorization: "Basic " + Buffer.from(credentials).toString("base64"),
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body,
-      method: "POST",
-    };
-
-    const response = await fetch(
-      `https://oauth-e2e.platform.intuit.com/oauth2/v1/tokens/bearer`,
-      options
-    );
-
-    if (!response.ok) {
-      return { error: "Failed to fetch token", status: 500 };
-    }
-
-    const data = await response.json();
-    await new Promise((resolve) => {
-      // Added 3 seconds to test the server side rendering and display of loader
-      setTimeout(() => {
-        resolve("");
-      }, 1000);
-    });
-    return { status: 200, token: data.access_token };
-  } catch (error) {
-    console.log(JSON.stringify(error));
-    return { error: "Failed to fetch token", status: 500 };
-  }
 }
 
 export default async function OffersPage() {
-  const { status, token } = await getBearerToken();
+    const { status, token } = await getBearerToken();
+    console.log(token);
 
   if (status === 200 && token) {
     return <OfferPageWrapper token={token} />;
