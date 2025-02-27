2e05bbcfd478b79c4e0a5ae2e7721df65e535c2f
epgtechteam <epg.techteam@gmail.com>
2025-02-27 10:16:32 -0800

    feat: release develop (#23)
    * feat: server side development (#4)
    
    * feat: added default and home page redirection logic
    
    * feat: added server endpoint to get bearer token
    
    * feat: added persona types and props
    
    * feat: added WidgetContainer placeholder
    
    * feat: build failing, trial fix
    
    * feat: added base url to .env file
    
    * feat: test changes
    
    * feat: test changes 2
    
    * feat: test changes 3
    
    * feat: test changes 3
    
    * feat: added vercel url for deployment
    
    * feat: added vercel url for deployment 1
    
    * feat: added vercel url for deployment 2
    
    * feat: added vercel url for deployment 3
    
    * feat: iframe integration good to go (#7)
    
    * feat: iframe integration good to go
    
    * feat: fix build
    
    * fix: ignore no-sync-scripts performance linting
    
    * fix: update widget container to jsx
    
    ---------
    
    Co-authored-by: George Mbugua <george_mbugua@intuit.com>
    
    * feat: added UI and persona selection (#8)
    
    * feat: added UI and persona selection
    
    * feat: updated margin above application steps
    
    * feat: passing widget props, fully working build
    
    * feat: fix build error
    
    * feat: fixed height
    
    * feat: succesful integration with widget, stable build
    
    * feat: updated payload, optimized widget container rendering
    
    * added dynamic estimate details, border to iframe (#19)
    
    * fix: prevent fetch request caching
    
    * feat: use environment variables for ck url params (#22)
    
    Co-authored-by: George Mbugua <george_mbugua@intuit.com>
    
    ---------
    
    Co-authored-by: George Mbugua <george_mbugua@intuit.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,67 +3,67 @@ import "./globals.css";
 import Header from "../components/Header";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>Breeze Wizz</title>
-        <script
-          type="text/javascript"
-          src="https://appf-web-exp-integration-e2e.app.intuit.com/v1/appfabric-web-integration/iframe/script/iife/appf-web-exp-integration-webapp"
-        />
-        <script>
-          {` 
-                        const { IntuitWebAppExperience } = globalThis.IntuitWeb;
-                        const widget = new IntuitWebAppExperience();
-                        (
-                          async () => widget.init({
-                              namespace: "embedded-financing",
-                              url: new URL(
-                                  "https://financing-e2e.app.intuit.com/embedded-financing?darwinPackageVersion=3.2.0-alpha.73bf954&driToken=eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjay1pbnRlcm5hbCIsImNyZWF0ZWRfYnkiOiJlbnYtdG9vbCIsImRhdGEiOnsiZGVwbG95X3NlZ21lbnQiOiJnbS1pbnR1aXQtZGVtby1idyJ9LCJlbnYiOiJDa1Rlc3RTZWdtZW50IiwiZXhwIjoxNzQxMjAyMTA3LCJpYXQiOjE3NDAwMDIxMDcsImlzcyI6ImVudi10b29sIiwianRpIjoiNzE5NThkMTEtYmQ2Mi00NmQxLWJkM2QtMWMwODUzY2ZiYWZmIn0."
-                              ),
-                              style: {
-                                  height: "100%",
-                                  width: "100%",
-                                  "border-color": "red",
-                              },
-                              props: {
-                                  onSuccess: (type, message, additionalInfo) => {
-                                    console.log(type, message, additionalInfo);
-                                  },
-                                  onError: (type, errorCode, message, additionalInfo) => {
-                                    console.log(type, errorCode, message, additionalInfo);
-                                  },
-                                  onEvent: (type, message, additionalInfo) => {
-                                    console.log(type, message, additionalInfo);
-                                  }
-                              }
-                          })
-                        )();
-                        globalThis.IntuitWebAppExperience = widget;
-                      `}
-        </script>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {<Header />}
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <title>Breeze Wizz</title>
+                <script
+                    type="text/javascript"
+                    src="https://appf-web-exp-integration-e2e.app.intuit.com/v1/appfabric-web-integration/iframe/script/iife/appf-web-exp-integration-webapp"
+                />
+                <script>
+                    {` 
+              const { IntuitWebAppExperience } = globalThis.IntuitWeb;
+              const widget = new IntuitWebAppExperience();
+              (
+                async () => widget.init({
+                    namespace: "embedded-financing",
+                    url: new URL(
+                        "https://financing-e2e.app.intuit.com/embedded-financing?darwinPackageVersion=${process.env.CK_DARWIN_PACKAGE_VERSION}&driToken=${process.env.CK_DRI_TOKEN}"
+                    ),
+                    style: {
+                        height: "100%",
+                        width: "100%",
+                        "border-color": "red",
+                    },
+                    props: {
+                        onSuccess: (type, message, additionalInfo) => {
+                          console.log(type, message, additionalInfo);
+                        },
+                        onError: (type, errorCode, message, additionalInfo) => {
+                          console.log(type, errorCode, message, additionalInfo);
+                        },
+                        onEvent: (type, message, additionalInfo) => {
+                          console.log(type, message, additionalInfo);
+                        }
+                    }
+                })
+              )();
+              globalThis.IntuitWebAppExperience = widget;
+            `}
+                </script>
+            </head>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {<Header />}
+                {children}
+            </body>
+        </html>
+    );
 }
